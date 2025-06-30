8e6e1fff8dafd2d1082b6dd9b3c7b94ff58e2c2b
KHARBOUCH MOHAMMED AMINE <kharbouch.mohammed.amine@gmail.com>
2025-06-30 15:47:34 +0000

    change the icon in "GET IT ON
    Google Play"

--- src/components/landing/cta.tsx
+++ src/components/landing/cta.tsx
@@ -1,16 +1,6 @@
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
-
-const GooglePlayIcon = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg viewBox="0 0 512 512" fill="currentColor" {...props}>
-      <path d="M31.25,13.56,178,160.31,31.25,307.06Z" style={{fill:'#00c0f3'}}/>
-      <path d="M480.75,256,357,379.75,210.25,233,357,82.25Z" style={{fill:'#ffdd00'}}/>
-      <path d="M178,160.31,210.25,233,31.25,307.06Z" style={{fill:'#ff3d00'}}/>
-      <path d="m31.25,13.56,146.75,146.75,302.75,95.69,0-194.44Z" style={{fill:'#00e57a'}}/>
-      <path d="M480.75,256,357,132.25,210.25,283,357,429.75Z" style={{fill:"#00a359"}}/>
-    </svg>
-);
-
+import { Play } from 'lucide-react';
 
 export default function CallToAction() {
   return (
@@ -22,7 +12,7 @@ export default function CallToAction() {
         </p>
         <Button size="lg" asChild className="h-16 px-6 bg-foreground text-background hover:bg-foreground/90 transition-transform hover:scale-105">
           <Link href="#" className="inline-flex items-center">
-            <GooglePlayIcon className="h-8 w-8 mr-3" />
+            <Play className="h-8 w-8 mr-3" />
             <div className="text-left">
               <p className="text-xs">GET IT ON</p>
               <p className="text-xl font-semibold -mt-1">Google Play</p>
